17a85dea8489d35f4436e2fb2707541d870b343f
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-07 01:30:11 -0700

    Added text file and created iterative and recursion functions

--- fib.c
+++ fib.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   printf("Hello!\n");
+int FibIterative(int n) {
+   if (n == 1) {
+      return 0;
+   } else if (n == 2) {
+      return 1;
+   }
+   int first = 0, second = 1, next_value;
+   for (int i = 3; i <= n; i++) {
+      next_value = first + second;
+      first = second;
+      second = next_value;
+   }
+   return second;
+}
+
+int FibRecursive(int n) {
+   if(n == 1){
+      return 0;
+   } else if (n == 2) {
+      return 1;
+   } else {
+      return (FibRecursive(n-1) + FibRecursive(n-2));
+   }
+}
+
+int main(int argc, char *argv[]) {
+   int value = atoi(argv[1]);
+   FILE *text_file = fopen(argv[3], "r");
+   int file_value;
+   fscanf(text_file, "%d", &file_value);
+   fclose(text_file);
+
+   int total = value + file_value;
+
+   if (argv[2][0] == 'i') {
+      int result = FibIterative(total);
+      printf("%d", result);
+   } else if (argv[2][0] == 'r') {
+      int result = FibRecursive(total);
+      printf("%d", result);
+   }
    return 0;
+
 }
