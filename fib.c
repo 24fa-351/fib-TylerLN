8b57365381ccd36251b41e0ee51b9c6245e2a1b2
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-14 15:22:46 -0700

    Memoizations

--- fib.c
+++ fib.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+unsigned long long int i_memo_array[100000000];
+unsigned long long int r_memo_array[100000000];
+
+
 unsigned long long int FibIterative(int n) {
+   if (i_memo_array[n] != 0) {
+      return i_memo_array[n];
+   }
    if (n == 1) {
       return 0;
    } else if (n == 2) {
@@ -13,19 +21,32 @@ unsigned long long int FibIterative(int n) {
       first = second;
       second = next_value;
    }
+   i_memo_array[n] = second;
    return second;
 }
 
+
 unsigned long long int FibRecursive(int n) {
+   if (r_memo_array[n] != 0) {
+      return r_memo_array[n];
+   }
    if(n == 1){
       return 0;
    } else if (n == 2) {
       return 1;
-   } else {
-      
-      return (FibRecursive(n-1) + FibRecursive(n-2));
    }
+   r_memo_array[n] = FibRecursive(n-1) + FibRecursive(n-2);
+   return r_memo_array[n];
 }
+
+
+unsigned long long int fib_i_wrapper(int n) {
+   return FibIterative(n);
+}
+unsigned long long int fib_r_wrapper(int n) {
+   return FibRecursive(n);
+}
+
 
 int main(int argc, char *argv[]) {
    int value = atoi(argv[1]);
@@ -34,10 +55,12 @@ int main(int argc, char *argv[]) {
    fscanf(text_file, "%d", &file_value);
    fclose(text_file);
 
+
    int N = value + file_value;
 
+
    if (argv[2][0] == 'i') {
-     unsigned long long int result = FibIterative(N);
+      unsigned long long int result = FibIterative(N);
       printf("%llu", result);
    } else if (argv[2][0] == 'r') {
       unsigned long long int result = FibRecursive(N);
@@ -45,4 +68,6 @@ int main(int argc, char *argv[]) {
    }
    return 0;
 
+
 }
+
