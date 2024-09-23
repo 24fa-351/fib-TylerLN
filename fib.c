b4505c7a09caf05425878036910f0744ddb07029
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-23 10:59:11 -0700

    Format

--- fib.c
+++ fib.c
@@ -1,73 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ARRAY_SIZE 100
 
-
-unsigned long long int i_memo_array[100000000];
-unsigned long long int r_memo_array[100000000];
-
-
-unsigned long long int FibIterative(int n) {
-   if (i_memo_array[n] != 0) {
-      return i_memo_array[n];
-   }
-   if (n == 1) {
-      return 0;
-   } else if (n == 2) {
-      return 1;
-   }
-   unsigned long long int first = 0, second = 1, next_value;
-   for (int i = 3; i <= n; i++) {
-      next_value = first + second;
-      first = second;
-      second = next_value;
-   }
-   i_memo_array[n] = second;
-   return second;
-}
-
-
-unsigned long long int FibRecursive(int n) {
-   if (r_memo_array[n] != 0) {
-      return r_memo_array[n];
-   }
-   if(n == 1){
-      return 0;
-   } else if (n == 2) {
-      return 1;
-   }
-   r_memo_array[n] = FibRecursive(n-1) + FibRecursive(n-2);
-   return r_memo_array[n];
-}
-
+unsigned long long int memo_array[ARRAY_SIZE];
 
 unsigned long long int fib_i_wrapper(int n) {
-   return FibIterative(n);
-}
-unsigned long long int fib_r_wrapper(int n) {
-   return FibRecursive(n);
+  if (memo_array[n] != 0) {
+    return memo_array[n];
+  }
+  if (n == 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  }
+  unsigned long long int first = 0, second = 1, next_value;
+  for (int i = 3; i <= n; i++) {
+    next_value = first + second;
+    first = second;
+    second = next_value;
+  }
+  memo_array[n] = second;
+  return second;
 }
 
+unsigned long long int fib_r_wrapper(int n) {
+  if (memo_array[n] != 0) {
+    return memo_array[n];
+  }
+  if (n == 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  }
+  memo_array[n] = fib_r_wrapper(n - 1) + fib_r_wrapper(n - 2);
+  return memo_array[n];
+}
+
+unsigned long long int FibIterative(int n) { return fib_i_wrapper(n); }
+unsigned long long int FibRecursive(int n) { return fib_r_wrapper(n); }
 
 int main(int argc, char *argv[]) {
-   int value = atoi(argv[1]);
-   FILE *text_file = fopen(argv[3], "r");
-   int file_value;
-   fscanf(text_file, "%d", &file_value);
-   fclose(text_file);
+  int user_input = atoi(argv[1]);
+  FILE *text_file = fopen(argv[3], "r");
+  int file_value;
+  fscanf(text_file, "%d", &file_value);
+  fclose(text_file);
 
+  int N_total = user_input + file_value;
 
-   int N = value + file_value;
-
-
-   if (argv[2][0] == 'i') {
-      unsigned long long int result = FibIterative(N);
-      printf("%llu", result);
-   } else if (argv[2][0] == 'r') {
-      unsigned long long int result = FibRecursive(N);
-      printf("%llu", result);
-   }
-   return 0;
-
-
+  if (argv[2][0] == 'i') {
+    unsigned long long int result = fib_i_wrapper(N_total);
+    printf("%llu", result);
+  } else if (argv[2][0] == 'r') {
+    unsigned long long int result = fib_r_wrapper(N_total);
+    printf("%llu", result);
+  }
+  return 0;
 }
-
