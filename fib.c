21ae4a61c939b326cb46658a4cb7239c0b277ec6
TylerLN <tylerlngu@csu.fullerton.edu>
2024-11-20 00:10:44 -0800

    Clang

--- fib.c
+++ fib.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   printf("Hello!\n");
-   return 0;
+int FibIterative(int fib_index) {
+  if (fib_index == 1) {
+    return 0;
+  } else if (fib_index == 2) {
+    return 1;
+  }
+  int first = 0;
+  int second = 1;
+  int next_value;
+
+  for (int ix = 3; ix <= fib_index; ix++) {
+    next_value = first + second;
+    first = second;
+    second = next_value;
+  }
+  return second;
+}
+
+int FibRecursive(int recursive_index) {
+  if (recursive_index == 1) {
+    return 0;
+  } else if (recursive_index == 2) {
+    return 1;
+  } else {
+    return (FibRecursive(recursive_index - 1) + FibRecursive(recursive_index - 2));
+  }
+}
+
+int main(int argc, char *argv[]) {
+   if (argc != 4) {
+      printf("Usage: %s <value> <i/r> <file>\n", argv[0]);
+      return 1;
+   }
+
+  int value = atoi(argv[1]);
+  int file_value;
+  
+  FILE *text_file = fopen(argv[3], "r");
+  fscanf(text_file, "%d", &file_value);
+  fclose(text_file);
+
+  int N_total = value + file_value;
+
+  if (argv[2][0] == 'i') {
+    int result = FibIterative(N_total);
+    printf("%d", result);
+  } else if (argv[2][0] == 'r') {
+    int result = FibRecursive(N_total);
+    printf("%d", result);
+  }
+  return 0;
 }
